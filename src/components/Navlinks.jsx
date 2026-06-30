d0cd87f696939270c4b788b31ce9cc08886cd8c8
yassin nassrallah <yassinnassrallah@gmail.com>
2026-06-30 19:36:28 +0200

    Update navigation components

---
+++ src/components/Navlinks.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navlinks = () => {
+  return (
+    <nav>
+        <ul>
+            <li className='list-item'>Home</li>
+            <li className='list-item'>About</li>
+            <li className='list-item'>Projects</li>
+            <li className='list-item'>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navlinks

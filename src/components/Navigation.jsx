e3c440c0a7475f16ba0138f678e4f6a385423c3c
yassin nassrallah <yassinnassrallah@gmail.com>
2026-06-30 19:30:08 +0200

    add nav container

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navlinks from './Navlinks'
+import Logo from './Logo'
+
+const Navigation = () => {
+  return (
+    <div className='nav-container'>
+      <Logo />
+      <Navlinks />
+    </div>
+  )
+}
+
+export default Navigation

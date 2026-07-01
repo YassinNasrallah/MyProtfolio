d42bd427fda017097a866fe0be3237f6241ef36e
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-01 16:46:04 +0200

    add hero content

---
+++ src/components/Hero/Herocontent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Herocontent = () => {
+  return (
+    <div className='hero-section'>
+       <h1>Im yassin
+        <br />im frontend developer, game programmer and game designer
+       </h1>
+    </div>
+  )
+}
+
+export default Herocontent

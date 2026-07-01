5e1da229fb1ade6e4be0e830d2fbaa52718bf21f
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-01 16:58:04 +0200

    Add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navigation from '../components/navigation/Navigation'
+import Herocontent from '../components/Hero/Herocontent'
+
+const Home = () => {
+  return (
+    <div className='home-section'>
+        <Navigation />
+        <Herocontent />
+      
+    </div>
+  )
+}
+
+export default Home

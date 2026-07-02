461023daf5987b5b91beee8991106281cd84b312
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-02 12:01:40 +0200

    add about section

---
+++ src/components/about/Aboutcontent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Aboutcontent = () => {
+  return (
+    <div className='about-section'>
+       <h1>About Me</h1>
+          Hi, I'm Yassin Nasrallah, a Junior Frontend Developer and Game Developer.
+          I've always been curious about how games are made,
+          and that curiosity inspired me to start learning programming.
+          Today, I enjoy building modern web applications with React and creating interactive game
+          experiences while continuously learning and improving my skills.
+    </div>
+  )
+}
+
+export default Aboutcontent

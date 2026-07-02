0e99d729da3efb6dc5afc6b75cf6b2ad07ad0ad7
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-02 12:03:44 +0200

    add about section to main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navigation from '../components/navigation/Navigation'
 import Herocontent from '../components/Hero/Herocontent'
+import Aboutcontent from '../components/about/Aboutcontent'
 
 const Home = () => {
   return (
     <div className='home-section'>
         <Navigation />
         <Herocontent />
-      
+         <Aboutcontent />
     </div>
   )
 }

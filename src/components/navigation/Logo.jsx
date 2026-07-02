87cc80a0bf6f55691b14d63c6eabad6cb73b4de7
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-02 13:13:58 +0200

    add profile

--- src/components/navigation/Logo.jsx
+++ src/components/navigation/Logo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Profile from '../assest/profile.jfif'
+import Profile from '../../assest/profile.jfif'
+
 const Logo = () => {
   return (
     <div className='profile'>

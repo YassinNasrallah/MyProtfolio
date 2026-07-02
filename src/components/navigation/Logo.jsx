252de08c3803c176f87529ae7e18155ba4ed328f
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-02 13:09:36 +0200

    add profile image

--- src/components/navigation/Logo.jsx
+++ src/components/navigation/Logo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Profile from '../assest/profile.jfif'
 const Logo = () => {
   return (
-    <div>
-       <img src="" alt="" />
+    <div className='profile'>
+       <img src={Profile} alt="" />
     </div>
   )
 }

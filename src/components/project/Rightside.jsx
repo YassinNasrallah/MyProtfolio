74284564521ccbc43676f747b232d862ddbf8e29
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-04 13:36:17 +0200

    add right side content

---
+++ src/components/project/Rightside.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Rightside = () => {
+  return (
+    <div className='righ-side'>
+      <h1>Project Heighlight</h1>
+      <ul>
+        <li>indie game</li>
+        <li>godot engine</li>
+        <li>Gdot script</li>
+        <li>itch.io</li>
+      </ul>
+      <a href="http://">learn more</a>
+    </div>
+  )
+}
+
+export default Rightside

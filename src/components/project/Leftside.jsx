94a51a6989ecde6289d28f3db42825585aa9c90d
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-04 13:39:58 +0200

    add leftside content

---
+++ src/components/project/Leftside.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import leftsideimage from '../../assest/leftsideimage.png'
+const Leftside = () => {
+  return (
+    <div className='left-side'>
+      <img src={leftsideimage} alt="" />
+    </div>
+  )
+}
+
+export default Leftside

e26346dbc699a384b423a466d4ddc6c06c93dd50
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-04 13:45:07 +0200

    add section folder

---
+++ src/section/Projectssection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Leftside from '../components/project/Leftside'
+import Rightside from '../components/project/Rightside'
+
+const Projectssection = () => {
+  return (
+    <div className='projects-section'>
+       <Leftside />
+       <Rightside />
+    </div>
+  )
+}
+
+export default Projectssection

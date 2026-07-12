ab66ac9faf0cb10379cde5e3359c85f9f71d4fda
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-12 18:27:30 +0200

    merge with project section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Home from './pages/Home';
+import ProjectHighlights from './pages/ProjectHighlights';
 
 function App() {
   return (
     <div className="App">
        <Home />
+       <ProjectHighlights />
     </div>
   );
 }

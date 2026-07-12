f17891a8c967d333180b121a5bd7be2f71bd0836
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-12 18:01:41 +0200

    add project highlight

---
+++ src/pages/ProjectHighlights.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from '../components/navigation/Navigation'
+
+const ProjectHighlights = () => {
+  return (
+    <div className='project-container'>
+       <Navigation />
+    </div>
+  )
+}
+
+export default ProjectHighlights

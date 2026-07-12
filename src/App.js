347df041a335fdd0073acd2601bca703cfc9e07c
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-12 19:50:57 +0200

    update routers and add scrolltotop using useffect

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import ProjectHighlights from './pages/ProjectHighlights';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/project-highlights' element={<ProjectHighlights />}/>

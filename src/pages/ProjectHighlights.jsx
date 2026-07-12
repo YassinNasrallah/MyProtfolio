1d23f58e31ffda0e10bb67b3742eda2c1d2caf6e
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-12 18:20:14 +0200

    add css file to project page

--- src/pages/ProjectHighlights.jsx
+++ src/pages/ProjectHighlights.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navigation from '../components/navigation/Navigation'
 import Aboutproject from '../components/about/Aboutproject'
-
+import '../style/projectssection.css'
 const ProjectHighlights = () => {
   return (
     <div className='project-container'>

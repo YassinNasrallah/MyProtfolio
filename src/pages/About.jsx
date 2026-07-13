46dedb3be961b00bbf413fdc1e0301784194b8aa
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-13 14:08:51 +0200

    add about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Aboutme from '../components/about/Aboutme'
+
+const About = () => {
+  return (
+    <div className='about-container'>
+       <Aboutme />
+    </div>
+  )
+}
+
+export default About

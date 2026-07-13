54651059bb5c5d100619dc3c911b87df1f4d6166
yassin nassrallah <yassinnassrallah@gmail.com>
2026-07-13 13:54:36 +0200

    add about me section with prestation and my skills

---
+++ src/components/about/Aboutme.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Aboutme = () => {
+  return (
+    <div className='about-section'>
+       <h2>Hllo,</h2>
+
+       <p>
+       I'm Yassin Nasrallah, a junior Front-End Developer,
+   Game Programmer, and Game Designer. My passion 
+   for technology started with video games. I began playing
+    games at the age of 12, and one of the first games that
+     truly inspired me was Resident Evil 4 (Classic). 
+     I remember being excited to get home from school every day just to continue my adventure.
+      That passion eventually motivated me to learn programming and start creating games
+       of my own. Today, I enjoy building modern web applications with React and developing
+        2D games in Godot while continuously improving my skills.
+</p>
+<div className="skills">
+
+<h2>My Skills</h2>
+<p>
+   I enjoy learning new technologies and improving my skills every day.
+   I believe continuous learning is an important part of becoming a better developer. 
+   I'm a good listener, and I'm always eager to take on new challenges.
+</p>
+
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>JavaScript</li>
+  <li>React</li>
+  <li>Godot</li>
+  <li>Git & GitHub</li>
+</ul>
+</div>
+
+    </div>
+  )
+}
+
+export default Aboutme
